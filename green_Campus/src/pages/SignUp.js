33dec63a13ecc5fe048642675420d2f9fee2c023
shubham-gravitii <shubham@gravitii.xyz>
2024-02-06 04:27:52 +0530

    Sign Up form color correction

--- green_Campus/src/pages/SignUp.js
+++ green_Campus/src/pages/SignUp.js
@@ -1,26 +1,23 @@
-import { useState ,useContext,useEffect} from "react";
+import { useState, useContext, useEffect } from "react";
 
-import { Link ,useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
-import AlertContext from '../Context/Alert/AlertContext'
-import LoaderContext from '../Context/LoaderContext'
-
-
-
+import AlertContext from "../Context/Alert/AlertContext";
+import LoaderContext from "../Context/LoaderContext";
 
 export default function SignUp() {
   const navigate = useNavigate();
   useEffect(() => {
-    if(Auth.loggedIn()){
-      navigate('/')
+    if (Auth.loggedIn()) {
+      navigate("/");
     }
-  })
+  });
   const context = useContext(AlertContext);
-  const {  setnotificationMsg } = context;
-  const context1=useContext(LoaderContext);
-  const {setisLoading}=context1;
+  const { setnotificationMsg } = context;
+  const context1 = useContext(LoaderContext);
+  const { setisLoading } = context1;
   const [formState, setFormState] = useState({
     username: "",
     email: "",
@@ -28,7 +25,7 @@ export default function SignUp() {
   });
 
   const [addUser, { error }] = useMutation(ADD_USER);
-  console.log(error)
+  console.log(error);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -50,186 +47,97 @@ export default function SignUp() {
       });
 
       Auth.login(data.addUser.token);
-      setnotificationMsg("Sign Up Successfull")
-      navigate('/calculator')
+      setnotificationMsg("Sign Up Successfull");
+      navigate("/calculator");
     } catch (e) {
-      setnotificationMsg(e.message)
+      setnotificationMsg(e.message);
       console.error(e.message);
     }
-    setisLoading(false)
+    setisLoading(false);
   };
 
   return (
-    // <ThemeProvider theme={defaultTheme}>
-    //   <Container component="main" maxWidth="xs">
-    //     <CssBaseline />
-    //     <Box
-    //       sx={{
-    //         marginTop: 8,
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         alignItems: "center",
-    //       }}
-    //     >
-    //       <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-    //         <LockOutlinedIcon />
-    //       </Avatar>
-    //       <Typography component="h1" variant="h5">
-    //         Sign up
-    //       </Typography>
-    //       <Box
-    //         component="form"
-    //         noValidate
-    //         onSubmit={handleFormSubmit}
-    //         sx={{ mt: 3 }}
-    //       >
-    //         <Grid container spacing={2}>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               autoComplete="given-name"
-    //               name="username"
-    //               required
-    //               fullWidth
-    //               id="firstName"
-    //               label="Name of the College"
-    //               autoFocus
-    //               value={formState.username}
-    //               onChange={handleChange}
-    //             />
-    //           </Grid>
-
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               required
-    //               fullWidth
-    //               id="email"
-    //               label="Email Address"
-    //               name="email"
-    //               autoComplete="email"
-    //               value={formState.email}
-    //               onChange={handleChange}
-    //             />
-    //           </Grid>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               required
-    //               fullWidth
-    //               name="password"
-    //               label="Password"
-    //               type="password"
-    //               id="password"
-    //               autoComplete="new-password"
-    //               value={formState.password}
-    //               onChange={handleChange}
-    //             />
-    //           </Grid>
-              
-    //         </Grid>
-    //         <Button
-    //           type="submit"
-    //           fullWidth
-    //           variant="contained"
-    //           sx={{ mt: 3, mb: 2 }}
-    //         >
-    //           Sign Up
-    //         </Button>
-    //         <Grid container justifyContent="flex-start">
-    //           <Grid item>
-    //             <Link to="/login" variant="body2">
-    //               Already have an account? Sign in
-    //             </Link>
-    //           </Grid>
-    //         </Grid>
-    //       </Box>
-    //     </Box>
-    //   </Container>
-    // </ThemeProvider>
     <div>
-            <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
-                <div>
-                    
-                        <h3 className="text-4xl font-bold text-purple-600">
-                            Sign Up
-                        </h3>
-                    
-                </div>
-                <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
-                    <form className="p-3"> 
-                        <div>
-                            <label
-                                htmlFor="name"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Name of the College
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    required={true}
-                                    type="text"
-                                    name="username"
-                                    value={formState.username}
-                                    onChange={handleChange}
-                                    className="p-2 block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Email
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    required={true}
-
-                                    type="email"
-                                    name="email"
-                                    value={formState.email}
-                                    onChange={handleChange}
-                                    className="p-2 block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <label
-                                htmlFor="password"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Password
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    required={true}
-                                    type="password"
-                                    onChange={handleChange}
-                                    value={formState.password}
-                                    name="password"
-                                    className="p-2 block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        
-                        
-                        <div className="flex items-center mt-4">
-                            <button onClick={handleFormSubmit}
-                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                                Register
-                            </button>
-                        </div>
-                    </form>
-                    <div className="mt-4 text-grey-600">
-                        Already have an account?{" "}
-                        <span>
-                            <Link className="text-purple-600 hover:underline" to="/login">
-                                Log in
-                            </Link>
-                        </span>
-                    </div>
-                    
-                </div>
-            </div>
+      <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-[#d1d1d1]">
+        <div>
+          <h3 className="text-4xl font-bold text-purple-600">Sign Up</h3>
         </div>
+        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-[#efefef] shadow-md sm:max-w-lg sm:rounded-lg">
+          <form className="p-3">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-md font-medium text-gray-700 undefined"
+              >
+                Name of the College
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  required={true}
+                  type="text"
+                  name="username"
+                  value={formState.username}
+                  onChange={handleChange}
+                  className="p-2 bg-[#f9f9f9] block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="email"
+                className="block text-md font-medium text-gray-700 undefined"
+              >
+                Email
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  required={true}
+                  type="email"
+                  name="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                  className="p-2 bg-[#f9f9f9] block w-full mt-1 border-gray-500 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="password"
+                className="block text-md font-medium text-gray-700 undefined"
+              >
+                Password
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  required={true}
+                  type="password"
+                  onChange={handleChange}
+                  value={formState.password}
+                  name="password"
+                  className="p-2 block w-full bg-[#f9f9f9] mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center mt-4">
+              <button
+                onClick={handleFormSubmit}
+                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+          <div className="mt-4 text-grey-600">
+            Already have an account?{" "}
+            <span>
+              <Link className="text-purple-600 hover:underline" to="/login">
+                Log in
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
